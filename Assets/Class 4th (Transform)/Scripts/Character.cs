6920bc560eed801dd7991da283eeb36e0b06e49d
foxvox <windev@nate.com>
2025-09-08 15:42:47 +0900

    0908
    0908

--- Assets/Class 4th (Transform)/Scripts/Character.cs
+++ Assets/Class 4th (Transform)/Scripts/Character.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    private float moveSpeed = 20f; 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 20f; 
+    
     void Start()
     {      
     } 
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal"); 
-        float v = Input.GetAxis("Vertical");    
+        float h = Input.GetAxisRaw("Horizontal"); 
+        float v = Input.GetAxisRaw("Vertical");    
                 
-        Vector3 dir = new Vector3(h, 0f, v).normalized;
+        Vector3 dir = new Vector3(h, 0f, v).normalized; 
         
-        transform.Translate(dir * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(dir * moveSpeed * Time.deltaTime, Space.Self);         
     }
 }
